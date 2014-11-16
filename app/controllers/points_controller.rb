362f4b972735ffa09073bb590de53056715b65c1
Joseph Robinson <joer1985@gmail.com>
2014-11-16 16:08:05 -0500

    Refactored iframe url

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -5,5 +5,12 @@ class PointsController < ApplicationController
 
   def show
     @point = Point.find(params[:id])
+    @url = iframe_url
+  end
+
+  private
+
+  def iframe_url
+    "https://www.google.com/maps/embed/v1/place?key=#{ENV["GOOGLE_MAPS_API_KEY"]}&q=#{@point.latitude},#{@point.longitude}"
   end
 end
